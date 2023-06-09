aeb06f050329f4ab70b320201601f5403974153e
Elizandra <monteiroelizandra2017@gmail.com>
2023-06-08 23:02:56 -0300

    tada: initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,19 @@
+function toggleMode(){
+const html= document.documentElement
+html.classList.toggle('light')
+
+//pegar tag img 
+const img = document.querySelector("#profile img")
+
+//substituir a imagem
+if(html.classList.contains('light')) {
+   //se tiver light mode add img light
+   img.setAttribute('src', './assets/avatar.light.png')
+} else {
+//se tiver sem light mode, manter a img normal
+img.setAttribute('src', './assets/avatar.png')
+}
+
+}
+
+
